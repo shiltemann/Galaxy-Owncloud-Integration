4b20e8d25e410fc92704e7dbd76cfb93406df108
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-03-30 01:00:56 +0530

    Updated to handle cloudstor url structure

--- Galaxy_SendToOwncloud/owncloud_send.sh
+++ Galaxy_SendToOwncloud/owncloud_send.sh
@@ -1,15 +1,14 @@
-
-# owncloud_send.sh "$server/remote.php/webdav/$filename"  $__user_email__ $mypassb64 "$inputfile" 
-filename=$1
-email=$2
-mypassb64=$3
+# owncloud_send.sh "$server/remote.php/webdav/$filename"  $__user_email__ $mypassb64 "$inputfile"
+server_url=$1
+username_b64=$2
+password_b64=$3
 mydatafile=$4
+filename_b64=$5
 
-# curl -X PUT "$server/remote.php/webdav/$filename" -u $__user_email__:$mypass --data-binary @"$inputfile" 
+username=$(echo $username_b64 | base64 --decode)
+password=$(echo $password_b64 | base64 --decode)
+filename=$(echo $filename_b64 | base64 --decode)
 
-# decode b64 encoded password
-echo -n "$mypassb64" > tmpfileb64
-mypass=`base64 -d tmpfileb64`
-
+echo curl -X PUT $server_url/$username/$filename -u $username:$password --data-binary @"$mydatafile" > /tmp/owncloud.txt
 # send file to owncloud
-curl -X PUT $filename -u $email:$mypass --data-binary @"$mydatafile" 
+curl -X PUT $server_url/$username/$filename -u $username:$password --data-binary @"$mydatafile"
